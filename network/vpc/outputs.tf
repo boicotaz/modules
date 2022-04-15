15823ceb0dfdec3441779052f462678eb0df05b8
boicotaz <tolisgerodimos94@gmail.com>
2022-04-15 20:47:28 +0300

    Add module outputs for vpc, default sg and public subnet

--- network/vpc/outputs.tf
+++ network/vpc/outputs.tf
@@ -1,4 +1,24 @@
-output "debug" {
-  value       = data.aws_availability_zones.available.names
-  description = "The names of the available zones in this AWS account for specific region"
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = try(aws_vpc.main.id, "")
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = try(aws_vpc.main.arn, "")
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = try(aws_vpc.main.default_security_group_id, "")
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_arns" {
+  description = "List of ARNs of public subnets"
+  value       = aws_subnet.public[*].arn
 }
